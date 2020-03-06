9cb69d51d291bf14b68f7567df5baf3f9389c166
bbkssh <byksh_shr@live.com>
2020-03-05 20:25:35 -0600

    fileUpload + return custom pojo/object, file output

--- src/main/java/s3/january/session/api/BalanceInquiryApi.java
+++ src/main/java/s3/january/session/api/BalanceInquiryApi.java
@@ -1,11 +1,14 @@
 package s3.january.session.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import s3.january.session.api.model.LoanRequest;
 import s3.january.session.service.BalanceInquiryService;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("CITI/")
@@ -17,5 +20,35 @@ public class BalanceInquiryApi {
     @GetMapping("/currentBalance")
     public int currentBalance() {
         return balanceInquiryService.getCurrentBalance();
+    }
+
+
+
+    @PostMapping("/fileUpload")
+    public ResponseEntity<LoanRequest > fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+
+        /*
+        Things to do:
+
+        Uploaded the file = file
+        if you want to read the file, you need some objects to read e.g input stream
+
+        google: how to read multipart file and return it
+
+         */
+        String s = "sss";
+        //InputStream inputStream = new FileInputStream(file.getInputStream());
+        //10,20, 20, 20, 20
+        LoanRequest loanRequest = new LoanRequest();
+
+        loanRequest.setFirstName("Bikesh");
+        loanRequest.setLastNamew("shrestha");
+        loanRequest.setAddress("irving");
+        loanRequest.setIncome(2000);
+        loanRequest.setScore(750);
+        loanRequest.setSsn(12345);
+
+        return new ResponseEntity< LoanRequest >(loanRequest, HttpStatus.OK);
+
     }
 }
---
+++ src/main/java/s3/january/session/api/model/LoanRequest.java
@@ -0,0 +1,60 @@
+package s3.january.session.api.model;
+
+
+public class LoanRequest {
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNamew() {
+        return lastNamew;
+    }
+
+    public void setLastNamew(String lastNamew) {
+        this.lastNamew = lastNamew;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private int ssn;
+    private String firstName;
+    private String lastNamew;
+    private int score;
+    private double income;
+    private String address;
+}
