cde332865b44d0fdb57b3b67fcb290f1666e04b9
kumar shahi <shahi.sandeep1993@gmail.com>
2020-03-22 00:56:10 -0500

    alogorithms

---
+++ src/test/java/com/algorithms/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.algorithms.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	public int[] twoSum(int[] nums, int target) {
+
+
+	}
+}
