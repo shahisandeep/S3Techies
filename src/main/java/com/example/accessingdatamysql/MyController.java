67f4be8735ce3d2caa2179954e1a099ab5a32339
sandeep59 <Y742u877,./>
2020-10-15 18:27:58 -0500

    added more entities

---
+++ src/main/java/com/example/accessingdatamysql/MyController.java
@@ -0,0 +1,4 @@
+package com.example.accessingdatamysql;
+
+public class MyController {
+}
---
+++ src/main/java/com/example/accessingdatamysql/TestController.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    String test(){
+        return "hello";
+    }
+}
